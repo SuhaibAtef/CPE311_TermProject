15ab94988f7936cd64e814ec16291d218109d72f
Suhaib Atef <Suhaibatef20@gmail.com>
2020-12-27 02:06:05 +0200

    Fixing the menu

---
+++ Const.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CPE311_TermProject
+{
+    static class C
+    {
+        public static void StarPrint()
+        {
+            Console.WriteLine("*****************************************************");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,32 +5,62 @@ namespace CPE311_TermProject
 {
     class System
     {   
+
         static public void Login()
         {
             int choice;
 
-            Console.WriteLine("*****************************************************");
-            Console.WriteLine("*****************************************************");
-            Console.WriteLine("*****************************************************");
-            Console.WriteLine("\tLogin to Central Supply Unit System");
-            Console.WriteLine("1. Manager");
-            Console.WriteLine("2. Employee");
-            Console.WriteLine("3. Employee Sign up");
-            Console.WriteLine("4. Exit");
-            Console.WriteLine("*****************************************************");
-            Console.WriteLine("*****************************************************");
-            Console.WriteLine("Choice:");
-            choice = Convert.ToInt32(Console.ReadLine());
+            
+            
             while (true)
             {
-                if (choice == 1) { //managerSignIn(); 
+                C.StarPrint();
+                C.StarPrint();
+                C.StarPrint();
+                Console.WriteLine("\tLogin to Central Supply Unit System");
+                Console.WriteLine("1. Manager");
+                Console.WriteLine("2. Employee");
+                Console.WriteLine("3. Employee Sign up");
+                Console.WriteLine("4. Exit");
+                C.StarPrint();
+                C.StarPrint();
+                Console.Write("Choice:");
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (choice == 1 || choice == 2) {
+                    SignIn(); //managerSignIn(); 
+                    break;
+                }else if (choice == 3) {
+                    Console.WriteLine("EmployeeSignUp");//EmployeeSignUp();
                 }
-                if (choice == 2) { //employeeSignIn();
+                else if (choice == 4) { break; }
+                else
+                {
+                    Console.Write("Incorrect Choice, Please try again...");
+                    Login();
                 }
-                if (choice == 3) { //managerSignUp();
-                }
-                if (choice == 4) { break; }
             }
+        }
+
+        static public void SignIn() 
+        {
+            C.StarPrint();
+            C.StarPrint();
+            Console.Write("Enter Username:  ");
+            String username = Console.ReadLine();
+            Console.Write("Enter Password:  ");
+            String password = Console.ReadLine();
+            if (username == "Manager" && password == "Manager") 
+            {
+                Console.WriteLine("Manager Signed In Correctly");
+            }
+            else if (username == "Manager" && password == "Manager")
+            {
+                Console.WriteLine("Manager Signed In Correctly");
+            }
+            else {
+                Console.WriteLine("Wrong Login information try again or sign up if you are not");
+            }
+        
         }
         static void Main(string[] args)
            
